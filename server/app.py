5069dd4f36e315e7028c72a93be78dbdade124e5
legitatx <ryan.donaldson@simplify.jobs>
2022-09-24 13:21:22 -0500

    remove

--- server/app.py
+++ server/app.py
@@ -1,13 +1,8 @@
 from fastapi import FastAPI
 from .api.api_v1.api import api_router
-from .exceptions.carmotion import (
-    CarmotionException,
-    carmotion_exception_handler,
-)
 
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Carmotion Emotions API", version="0.1.0")
-    app.add_exception_handler(CarmotionException, carmotion_exception_handler)
     app.include_router(api_router, prefix="/v1")
     return app
