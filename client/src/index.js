56a45700d099094770c5337e854de8dc11d98751
Nirranjan Akilan <anirranjan@gmail.com>
2022-09-24 16:17:49 -0500

    Created graph component

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Landing from './Landing'
+import Graph from './Graph'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Landing/>
+    <Graph/>
   </React.StrictMode>
 );
 
