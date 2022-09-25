77e86e9b1938c64961c84335c0196b6e534fe298
legitatx <ryan.donaldson@simplify.jobs>
2022-09-25 02:38:48 -0500

    line chart

--- client/src/Graph.js
+++ client/src/Graph.js
@@ -1,21 +1,32 @@
-import {VictoryChart, VictoryStack, VictoryArea, VictoryTheme} from "victory"
-import "./App.css"
+import { VictoryChart, VictoryStack, VictoryArea, VictoryTheme } from "victory";
+import "./App.css";
 
 const data = [
-    {second: 1, positivityScore: 95},
-    {second: 2, positivityScore: 25},
-    {second: 3, positivityScore: -65},
-    {second: 4, positivityScore: 85},
-    {second: 5, positivityScore: 75},
-]
-function Graph(){
-    return (
-        <VictoryChart theme={VictoryTheme.material} animate={{duration: 1000}}>
-            <VictoryStack colorScale={"blue"}>
-                <VictoryArea data={data} x="second" y="positivityScore" interpolation={"basis"}/>
-            </VictoryStack>
-        </VictoryChart>
-    )
+  { second: 1, positivityScore: 95 },
+  { second: 2, positivityScore: 25 },
+  { second: 3, positivityScore: -65 },
+  { second: 4, positivityScore: 85 },
+  { second: 5, positivityScore: 75 },
+];
+
+function Graph() {
+  return (
+    <VictoryChart
+      theme={VictoryTheme.material}
+      animate={{
+        duration: 2000,
+        onLoad: { duration: 1000 },
+      }}
+    >
+      <VictoryLine
+        style={{
+          data: { stroke: "#c43a31" },
+          parent: { border: "1px solid #ccc" },
+        }}
+        data={data}
+      />
+    </VictoryChart>
+  );
 }
 
-export default Graph
+export default Graph;
