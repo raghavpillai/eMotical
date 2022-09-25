747fcb4b8dfc6dca046eef368dcc94c72066bebb
Raghav <raghavpillai101@gmail.com>
2022-09-24 22:58:54 -0500

    Revert "Deleted unneeded dependencies"
    This reverts commit eb6daeb2f73c98e4e05dcddd7bf715400105a5a8.

---
+++ server/api/api_v1/endpoints/emotion_router.py
@@ -0,0 +1,61 @@
+from typing import Any
+from fastapi import APIRouter, WebSocket
+from server.logic.session_handler import SessionHandler
+
+print("> Beginning build")
+router = APIRouter()
+s_handler: SessionHandler = SessionHandler()
+
+
+@router.get("/create/{session_id}")
+async def create_session(*, session_id: str) -> Any:
+    """
+    Creates a session given a session ID
+    """
+    s_handler.create_session(session_id)
+    return {"message": f"Created new session {session_id}"}
+
+
+@router.get("/update_entity/{category}/{url}/{amount}")
+async def update_entity(*, category: str, url: str, amount: str) -> Any:
+    """
+    Updates all weights for a category and url to a constant amount
+    """
+    if s_handler:
+        s_handler.update_entity(category, url, int(amount))
+        return {"success": True}
+    return {"success": False}
+
+
+@router.get("/get_recs/{category}")
+async def get_recs(*, category: str) -> Any:
+    """
+    Returns the top 5 recs (or random if no prev data) for a given category
+    """
+    if s_handler:
+        return s_handler.get_recs(category)
+    return {"success": False}
+
+
+@router.get("/end_session")
+async def process_image() -> Any:
+    """
+    Given a prompt to process image, process image and return data
+    """
+    if s_handler.current_session:
+        res = await s_handler.end_session()
+        return res
+    return False
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    """
+    Websocket to handle chatting for local session
+    """
+    SessionHandler.websocket_obj = WebSocket
+    await websocket.accept()
+    websocket.send_text(SessionHandler.prompt_chat_message())
+    while True:
+        data = await websocket.receive_text()
+        msg_to_send = SessionHandler.process_chat_msg(data)
+        await websocket.send_text(msg_to_send)
