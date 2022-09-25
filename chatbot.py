ffb6b928bd4afe81958006eece4df5e6c99c7f0b
bcohn13 <bcohn13@yahoo.com>
2022-09-24 19:47:42 -0500

    Cleaned the format up a little. Removed unneeded libraries.

--- chatbot.py
+++ chatbot.py
@@ -11,18 +11,18 @@ pip install git+https://github.com/PrithivirajDamodaran/Parrot.git
 pip install chatterbot
 """
 
+
 from textblob import TextBlob
 import time
 from random import choice
 time.clock = time.time
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer,ChatterBotCorpusTrainer
 from parrot import Parrot
 import torch
 import warnings
+
 warnings.filterwarnings("ignore")
 
-parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5", use_gpu=False)
+parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5", use_gpu=True)
 
 def response_feedback(input_text):
     """
@@ -33,13 +33,13 @@ def response_feedback(input_text):
     """
     
     if feedback_polarity<0:
-        return choice(parrot.augment("I'm sorry you feel that way"))[0].capitalize()+" What would you like to see? "
+        return choice(parrot.augment("I'm sorry you feel that way"))[0].capitalize()+". What would you like to see? "
     elif feedback_polarity>0:
-        return choice(parrot.augment("I'm glad you feel that way"))[0].capitalize()+" What makes you feel this way?"
+        return choice(parrot.augment("I'm glad you feel that way"))[0].capitalize()+". What makes you feel this way?"
 input_text=input("Hello, what do you think about this product?: ")
 feedback_polarity = TextBlob(input_text).sentiment.polarity
 while feedback_polarity==0:
     input_text=input("Please be more specific: ")
     feedback_polarity = TextBlob(input_text).sentiment.polarity
-input_text=input(response_feedback(input_text)+"")
+input_text=input(response_feedback(input_text))
 print(choice(parrot.augment("Thank you have a good day!"))[0].capitalize())
