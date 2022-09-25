fec5c3b5837ee4db604bb0d7ec36596aa008b351
legitatx <ryan.donaldson@simplify.jobs>
2022-09-25 06:02:40 -0500

    player

---
+++ client/src/Player.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import ReactPlayer from "react-player";
+import "./like.css";
+
+class ResponsivePlayer extends React.Component {
+  render() {
+    return (
+      <div className="player-wrapper">
+        <ReactPlayer
+          className="react-player"
+          url={this.props.url}
+          width="100%"
+          height="100%"
+        />
+      </div>
+    );
+  }
+}
